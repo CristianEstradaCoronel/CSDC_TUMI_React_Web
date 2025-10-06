b8f3d61b86e0eece6a75a2d878e9f4160d4e6dce
Cristian Estrada <cristian.estradacoronel@gmail.com>
2025-10-06 16:41:39 -0500

    06/10/25 16.41

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// DEPURAR LOCALMENTE
 export default defineConfig({
-  base: '/', // 👈 esto hace que funcione localmente
   plugins: [react()],
 })
-
-// DEPURAR EN GITHUB
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/CSDC_TUMI_React_Web/",
-// });
 
